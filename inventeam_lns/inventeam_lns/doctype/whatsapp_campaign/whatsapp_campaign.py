782f65e2560239f443fd4c647bb177d437e23a55
Mangesh Redij <mangesh@vmi1455331.contaboserver.net>
2023-11-09 15:33:15 +0530

    09112023 1530

--- inventeam_lns/inventeam_lns/doctype/whatsapp_campaign/whatsapp_campaign.py
+++ inventeam_lns/inventeam_lns/doctype/whatsapp_campaign/whatsapp_campaign.py
@@ -2,37 +2,42 @@
 # For license information, please see license.txt
 
 import frappe
+import json
 import datetime
 import requests
 from frappe.model.document import Document
-from frappe.integrations.utils import make_get_request
+from frappe.integrations.utils import make_post_request
 
-def enqueue_send_whatsapp_message(api_url, contact_number, templatename, keyword, message):
+def enqueue_send_whatsapp_message(api_url, wadata, contact_number, templatename, keyword):
     frappe.enqueue(
         'inventeam_lns.inventeam_lns.doctype.whatsapp_campaign.whatsapp_campaign.send_whatsapp_message',
         queue='short',
         job_name='Whatsapp Notification',
         api_url=api_url,
+        wadata = wadata,
         contact_number=contact_number,
         templatename=templatename,
-        keyword=keyword,
-        message=message
+        keyword=keyword
     )
 
 @frappe.whitelist()
-def send_whatsapp_message(api_url, contact_number, templatename, keyword, message):
+def send_whatsapp_message(api_url, wadata, contact_number, templatename, keyword):
     current_datetime = datetime.datetime.now()
     
-    response = make_get_request(
-            api_url
-        )
-    
+    headers = {
+        "content-type": "application/json"
+    }
+    response = make_post_request(
+        api_url,
+        headers=headers,
+        data=json.dumps(wadata)
+    )
     frappe.msgprint(response, alert=True)
     
     frappe.get_doc({
         "doctype": "Whatsapp Messages",
         "label": templatename,
-        "message": message,
+        "message": json.dumps(wadata),
         "to": contact_number,
         "keyword": keyword,
         "response": response,
@@ -53,13 +58,14 @@ class WhatsappCampaign(Document):
         wa_file = wa_template.file
         wa_message_type = wa_template.message_type
         
-        api_message_url = api_message_url.replace("{messagetype}", wa_message_type)
+        new_api_message_url = api_url + api_message_url
+        #api_message_url = api_message_url.replace("{messagetype}", wa_message_type)
         
-        if wa_message_type == "media":
-            api_message_url = f"{api_message_url}&media_url=https://lns.inventeam.in/{wa_file}&filename=" 
+        #if wa_message_type == "media":
+        #    api_message_url = f"{api_message_url}&media_url=https://lns.inventeam.in/{wa_file}&filename=" 
         
-        api_message_url = api_message_url.replace("{instanceid}", api_instanceid)
-        api_message_url = api_message_url.replace("{accesstoken}", api_accesstoken)
+        #api_message_url = api_message_url.replace("{instanceid}", api_instanceid)
+        #api_message_url = api_message_url.replace("{accesstoken}", api_accesstoken)
         
         sql_query = """
             SELECT Distinct `tabWhatsapp Contacts`.`mobileno`,`tabWhatsapp Contacts`.`contact_name` FROM `tabWhatsapp Contacts`
@@ -69,18 +75,38 @@ class WhatsappCampaign(Document):
         data = frappe.db.sql(sql_query, as_dict=True)
         
         i = 0
+        wadata = {}
         for row in data:
-            full_message = wa_message.replace("{$}", row.contact_name)
-            new_api_message_url = api_message_url
-            new_api_message_url = new_api_message_url.replace("{wamobileno}", "91" + row.mobileno)
-            new_api_message_url = new_api_message_url.replace("{wamessage}", full_message)
-            new_url = api_url + new_api_message_url
+            if wa_message_type == "media":
+                wadata = {
+                    "number": "91" + row.mobileno,
+                    "type": "media",
+                    "media_url": "https://lns.inventeam.in/" + wa_file,
+                    "message": wa_message.replace("{$}", row.contact_name),
+                    "instance_id": api_instanceid,
+                    "access_token": api_accesstoken
+                }
+            else:
+                wadata = {
+                    "number": "91" + row.mobileno,
+                    "type": "media",
+                    "media_url": "https://lns.inventeam.in/" + wa_file,
+                    "message": wa_message.replace("{$}", row.contact_name),
+                    "instance_id": api_instanceid,
+                    "access_token": api_accesstoken
+                }
+                
+            #full_message = wa_message.replace("{$}", row.contact_name)
+            #new_api_message_url = api_message_url
+            #new_api_message_url = new_api_message_url.replace("{wamobileno}", "91" + row.mobileno)
+            #new_api_message_url = new_api_message_url.replace("{wamessage}", full_message)
+            #new_url = api_url + new_api_message_url
             enqueue_send_whatsapp_message(
-                new_url,
+                new_api_message_url,
+                wadata,
                 row.mobileno,
                 template_name,
-                self.keywords,
-                full_message
+                self.keywords
             )
             i += 1
             
