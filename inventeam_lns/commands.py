33143f43e00119e11f2ca68951b330cb967dd828
Inventeam Solutions Pvt Ltd <ispl@vmi1922141.contaboserver.net>
2025-06-28 10:55:46 +0530

    28062025

--- inventeam_lns/commands.py
+++
@@ -1,7 +0,0 @@
-import frappe
-import click
-from inventeam_lns.inventeam_lns.doctype.whatsapp_campaign.whatsapp_campaign import get_single_whatsapp_contact
-
-@click.command('run-30-second-job')
-def run_30_second_job():
-    get_single_whatsapp_contact()
